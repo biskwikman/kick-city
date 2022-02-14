700140f7481302792836f4610f0800ad4cb384e0
Dan Henri <djosephhenri@gmail.com>
2022-02-14 12:49:22 +0900

    first commit

---
+++ src/Main.hx
@@ -0,0 +1,75 @@
+import hxd.Key;
+
+class Main extends hxd.App {
+
+    var bmp : h2d.Bitmap;
+    var velocity : Float = 5;
+    var gravity : Float = 1;
+    var jumpVelocity : Float = 10;
+    var jump : Bool = false;
+
+    var chunkSize : Float = 100;
+
+    override function init() {
+        // create red texture and 100 x 100 tile from it
+        var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
+        tile.dx = -tile.width / 2;
+        tile.dy = -tile.height / 2;
+
+        // create a bitmap object and add it to the default 2d scene (s2d)
+        bmp = new h2d.Bitmap(tile, s2d);
+
+        // modify display position of the bitmap
+        bmp.x = s2d.width * 0.5;
+        bmp.y = s2d.height *0.5;
+    }
+
+    function doJump() 
+    {
+        var jumpStartLoc : Float = bmp.y;
+        bmp.y -= jumpVelocity;
+        jumpVelocity -= gravity;
+        if( bmp.y == jumpStartLoc)
+        {
+            jumpVelocity = 0;
+            jump = false;
+        }
+    }
+
+    override function update(deltaTime:Float) {
+        // box falls 100 pixels per second
+        // bmp.y += velocity * chunkSize * deltaTime;
+        // acceleration
+        // velocity += gravity * deltaTime;
+
+        if ( jump == true )
+        {
+            doJump();
+        }
+
+        if( Key.isDown("W".code) ) 
+        {
+            bmp.y -= velocity;
+        }
+        if( Key.isDown("S".code) ) 
+        {
+            bmp.y += velocity;
+        }
+        if( Key.isDown("A".code) ) 
+        {
+            bmp.x -= velocity;
+        }
+        if( Key.isDown("D".code) ) 
+        {
+            bmp.x += velocity;
+        }
+        // jump
+        if( Key.isPressed(" ".code) ) 
+        {
+            jump = true;
+        }
+    }
+    static function main() {
+        new Main();
+    }
+}
